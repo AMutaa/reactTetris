69eb8c363204e68b42be8d1bcc9d1a7e7c3027ea
AMutaa <piusandru@gmail.com>
2019-08-29 23:31:05 -0400

    update

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -4,7 +4,7 @@ import Display from "./Display";
 import StartButton from "./StartButton";
 
 import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
-
+import { createStage } from "../gameHelpers";
 import { usePlayer } from "../Hooks/usePlayer";
 import { useStage } from "../Hooks/useStage";
 
@@ -16,9 +16,33 @@ const Tetris = () => {
   const [stage, setStage] = useStage();
 
   console.log("re-render");
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0 });
+  };
 
+  const startGame = () => {
+    // Reset Everything
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const dropPlayer = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer(1);
+      }
+    }
+  };
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -31,7 +55,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton onClick={startGame()} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
