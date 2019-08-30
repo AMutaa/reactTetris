77eede5647e2c4563a72cc695fc8c1cad7c27b0e
AMutaa <piusandru@gmail.com>
2019-08-29 23:13:48 -0400

    :update

--- src/Hooks/usePlayer.js
+++ src/Hooks/usePlayer.js
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { randomTetromino } from "../tetrominos";
 
 export const usePlayer = () => {
-  const [player, setPlayer] = useState();
-  const playerState = useState({
+  const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
     tetromino: randomTetromino().shape,
     collided: false
